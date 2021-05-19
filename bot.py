71bca9df035153cf7e9326fdb114895a3a66af4e
Ayush Jaiswal <69688537+ayushjaiswal1020@users.noreply.github.com>
2021-05-19 13:41:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,6 +22,22 @@ async def start(bot, message):
               "You can contact my boss using this bot!! \n\n Send your Message, I will Deliver it to my Boss.")
 
 
+from datetime import datetime
+from os import environ
+import aiohttp
+from pyrogram import Client, filters
+
+
+@bot.on_message(filters.command('ping') & filters.private)
+
+async def _(event):
+    start = datetime.now()
+    end = datetime.now()
+    ms = (end - start).microseconds / 1000
+    await asst.send_message(
+        event.chat_id,
+        f"**Pong!!**\n `{ms}ms`",
+    )
 
 
 bot.run()
