6ec80faaf972852989e86564b779144a640770c1
Ayush Jaiswal <69688537+ayushjaiswal1020@users.noreply.github.com>
2021-05-20 14:28:42 +0530

    Update bots.py

--- assistant/bots.py
+++ assistant/bots.py
@@ -1,1 +1,42 @@
 
+from os import environ
+import aiohttp
+from pyrogram import Client, filters
+from datetime import datetime
+
+API_ID = environ.get('API_ID')
+API_HASH = environ.get('API_HASH')
+BOT_TOKEN = environ.get('BOT_TOKEN')
+API_KEY = environ.get('API_KEY', 'c3398d623ffc9ab6de497e37d6938e280ed7a474')
+
+bot = Client('gplink bot',
+             api_id=API_ID,
+             api_hash=API_HASH,
+             bot_token=BOT_TOKEN,
+             workers=50,
+             sleep_threshold=10)
+
+
+@bot.on_message(filters.command('sarts') & filters.private)
+async def start(bot, message):
+    await message.reply(
+         f"**Hey there {message.chat.first_name}, this is Assistant of MJBhai!**\n\n"
+              "You can contact my boss using this bot!! \n\n Send your Message, I will Deliver it to my Boss.")
+
+
+
+
+
+@bot.on_message(filters.command('ping') & filters.private)
+
+async def _(event):
+    start = datetime.now()
+    end = datetime.now()
+    ms = (end - start).microseconds / 1000
+    await message.reply(
+        event.chat_id,
+        f"**Pong!!**\n `{ms}ms`",
+    )
+
+
+bot.run()
